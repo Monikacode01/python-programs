f0bcebdc5ca63e8abd3ba1829a7c6e6f62e28cd1
Monika V <vmonika0120@gmail.com>
2026-08-15 22:31:01 +0530

    Create addition.py

---
+++ addition.py
@@ -0,0 +1,11 @@
+# Addition of Two Numbers
+
+# Get two numbers from the user
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+# Add the two numbers
+addition = num1 + num2
+
+# Display the result
+print("Addition:", addition)
