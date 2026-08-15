2a71320a662bec9ccf757030bd36f16eced5240e
Monika V <vmonika0120@gmail.com>
2026-08-15 22:33:01 +0530

    Create multiplication.py

---
+++ multiplication.py
@@ -0,0 +1,11 @@
+# Multiplication of Two Numbers
+
+# Get two numbers from the user
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+# Multiply the two numbers
+multiplication = num1 * num2
+
+# Display the result
+print("Multiplication:", multiplication)
