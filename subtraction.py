860d747f574d4636569adf8c89b3de5effe8adbc
Monika V <vmonika0120@gmail.com>
2026-08-15 22:31:57 +0530

    Create subtraction.py

---
+++ subtraction.py
@@ -0,0 +1,11 @@
+# Subtraction of Two Numbers
+
+# Get two numbers from the user
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+# Subtract the second number from the first number
+subtraction = num1 - num2
+
+# Display the result
+print("Subtraction:", subtraction)
