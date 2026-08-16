63062691ec68797b7a65f5501813c68356a69678
Monika V <vmonika0120@gmail.com>
2026-08-16 22:32:24 +0530

    Create minimum_of_two_numbers.py

---
+++ minimum_of_two_numbers.py
@@ -0,0 +1,11 @@
+# Minimum of Two Numbers
+
+# Get two numbers from the user
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+
+# Compare the two numbers
+if a < b:
+    print("Minimum number is:", a)
+else:
+    print("Minimum number is:", b)
