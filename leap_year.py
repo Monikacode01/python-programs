fb069c41b6a9ffe350c011b1d8ddbc5d10140bef
Monika V <vmonika0120@gmail.com>
2026-08-16 22:37:30 +0530

    Create leap_year.py

---
+++ leap_year.py
@@ -0,0 +1,10 @@
+# Leap Year Program
+
+# Get the year from the user
+year = int(input("Enter a year: "))
+
+# Check whether the year is divisible by 4
+if year % 4 == 0:
+    print("It is a Leap Year")
+else:
+    print("It is not a Leap Year")
