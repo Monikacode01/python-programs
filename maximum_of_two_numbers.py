71bcb22ac069df39af9ea85c5e0ad27d5845f3d2
Monika V <vmonika0120@gmail.com>
2026-08-16 22:31:14 +0530

    Create maximum_of_two_numbers.py

---
+++ maximum_of_two_numbers.py
@@ -0,0 +1,11 @@
+# Maximum of Two Numbers
+
+# Get two numbers from the user
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+
+# Compare the two numbers
+if a > b:
+    print("Maximum number is:", a)
+else:
+    print("Maximum number is:", b)
