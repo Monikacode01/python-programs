f9b350acf646652d965a1359ea01a46e03c1c9b3
Monika V <vmonika0120@gmail.com>
2026-08-26 21:20:49 +0530

    Create rectangle_area.py

---
+++ rectangle_area.py
@@ -0,0 +1,8 @@
+# Command: Area of Rectangle = length × breadth
+
+length = float(input("Enter length: "))
+breadth = float(input("Enter breadth: "))
+
+area = length * breadth
+
+print("Area of Rectangle =", area)
