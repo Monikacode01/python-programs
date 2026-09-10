5c07a9a67d9c131ee16891afc7a82ffd7d66a4f8
Monika V <vmonika0120@gmail.com>
2026-09-10 22:01:33 +0530

    Create print_numbers_greater_than_10.py

---
+++ print_numbers_greater_than_10.py
@@ -0,0 +1,9 @@
+# Create a list of numbers
+numbers = [5, 15, 8, 20, 12, 3]
+
+#Use a for loop to check each number
+for number in numbers:
+ # Check if the number is greater than 10
+ if number > 10:
+   # Print the number if the condition is true
+   print(number)
