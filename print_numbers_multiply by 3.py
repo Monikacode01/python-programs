5373635605f494f2cc03523442e2ad99e0ec50e6
Monika V <vmonika0120@gmail.com>
2026-09-10 21:52:30 +0530

    Create print_numbers_multiply_by_3.py

---
+++ print_numbers_multiply by 3.py
@@ -0,0 +1,7 @@
+# Create a list of numbers
+numbers = [2, 4, 6, 8, 10]
+
+# Use a for loop to go through each number
+for number in numbers:
+    # Multiply each number by 3 and print the result
+    print(number * 3)
