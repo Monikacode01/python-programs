449317b20199bbab5acb87b54519df8cb19a0783
Monika V <vmonika0120@gmail.com>
2026-09-10 21:47:54 +0530

    Create print_each_fruit_name.py

---
+++ print_each_fruit_name.py
@@ -0,0 +1,7 @@
+# create a list of fruits
+fruits = ["Apple", "Banana", "Mango", "Orange"]
+
+# using a for loop to go through the list
+for fruit in fruits:
+    # print each fruit
+    print(fruit)
