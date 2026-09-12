de1acbc8e7d9fde76ae5b22e26ff9ed77fe3e112
Monika V <vmonika0120@gmail.com>
2026-09-12 21:49:35 +0530

    Create using_while_loop_password_finding.py

---
+++ using_while_loop_password_finding.py
@@ -0,0 +1,10 @@
+# Initialize password as an empty string
+password = "" 
+
+# Keep asking until the correct password is entered
+while password != "1234":  
+     # Get password from the user
+    password = input("Password: ") 
+
+# Display success message after correct password
+print("Login successful") 
