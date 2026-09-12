1a019ca99cd7f1d2a3cba0c01e4e03f2619d4e83
Monika V <vmonika0120@gmail.com>
2026-09-12 22:19:58 +0530

    Create sum_of_10_numbers_using_whileloop.py

---
+++ sum_of_10_numbers_using_whileloop.py
@@ -0,0 +1,13 @@
+# Initialize sum as 0
+sum = 0
+# Start from 1
+i = 1
+# Repeat until i reaches 10
+while i <= 10:
+    # Add the current number to sum
+    sum = sum + i
+    # Increase i by 1
+    i = i + 1
+    
+# Display the total
+print(sum)
