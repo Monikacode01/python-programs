15f5840dd4b6dfb390e6923f155ab515c6e16d06
Monika V <vmonika0120@gmail.com>
2026-09-12 22:30:30 +0530

    Create print_multiplication_table.py

---
+++ print_multiplication_table.py
@@ -0,0 +1,14 @@
+# Get a number from the user
+n = int(input("Enter a number: "))
+
+# Start the counter from 1
+i = 1
+
+# Repeat the loop while i is less than or equal to 10
+while i <= 10:
+
+    # Print the multiplication table
+    print(n, "x", i, "=", n * i)
+
+    # Increase i by 1
+    i += 1
