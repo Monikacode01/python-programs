4d253d5563f1e2010b2b16c00ca0c460ec369d1c
Monika V <vmonika0120@gmail.com>
2026-09-13 22:12:27 +0530

    Create calculate_circle_area.py

---
+++ calculate_circle_area.py
@@ -0,0 +1,10 @@
+# Area of Circle Program
+
+# Get the radius from the user
+radius = float(input("Enter the radius of the circle: "))
+
+# Calculate the area
+area = 3.14 * radius * radius
+
+# Display the result
+print("Area of the circle:", area)
