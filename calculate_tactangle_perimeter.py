ff01c4405696480c06ae7953b03223f41ca51e04
Monika V <vmonika0120@gmail.com>
2026-09-13 22:16:27 +0530

    Create calculate_tactangle_perimeter.py

---
+++ calculate_tactangle_perimeter.py
@@ -0,0 +1,9 @@
+# Get length and width from the user
+length = float(input("Enter length: "))
+width = float(input("Enter width: "))
+
+# Calculate the perimeter
+perimeter = 2 * (length + width)
+
+# Display the result
+print("Perimeter of Rectangle:", perimeter)
