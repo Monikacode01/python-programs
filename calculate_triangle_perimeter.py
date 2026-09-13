c6e024ba83312556254d165eb67e03b1f30c7510
Monika V <vmonika0120@gmail.com>
2026-09-13 22:40:16 +0530

    Create calculate_triangle_perimeter.py

---
+++ calculate_triangle_perimeter.py
@@ -0,0 +1,12 @@
+# Perimeter of Triangle Program
+
+# Get the three sides from the user
+side1 = float(input("Enter side 1: "))
+side2 = float(input("Enter side 2: "))
+side3 = float(input("Enter side 3: "))
+
+# Calculate the perimeter
+perimeter = side1 + side2 + side3
+
+# Display the result
+print("Perimeter of the triangle:", perimeter)
