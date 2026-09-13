a312e8540d3f9659c8c149495d1ba4894c7fb6ad
Monika V <vmonika0120@gmail.com>
2026-09-13 22:43:03 +0530

    Create calculate_square_perimeter.py

---
+++ calculate_square_perimeter.py
@@ -0,0 +1,8 @@
+# Get the side length from the user
+side = float(input("Enter side: "))
+
+# Calculate the perimeter
+perimeter = 4 * side
+
+# Display the result
+print("Perimeter of Square:", perimeter)
