537e7f53e3ddb24bf2d4735a44bdb517525d6045
Monika V <vmonika0120@gmail.com>
2026-09-13 22:37:05 +0530

    Create calculate_square_area.py

---
+++ calculate_square_area.py
@@ -0,0 +1,10 @@
+# Area of Square Program
+
+# Get the side from the user
+side = float(input("Enter the side of the square: "))
+
+# Calculate the area
+area = side * side
+
+# Display the result
+print("Area of the square:", area)
