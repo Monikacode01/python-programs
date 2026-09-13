1872102ae4815b814e251e561a9bfdaac61226f7
Monika V <vmonika0120@gmail.com>
2026-09-13 22:03:55 +0530

    Create calculate_triangle_area.py

---
+++ calculate_triangle_area.py
@@ -0,0 +1,9 @@
+# Get base and height from the user
+base = float(input("Enter base: "))
+height = float(input("Enter height: "))
+
+# Calculate the area
+area = 0.5 * base * height
+
+# Display the result
+print("Area of Triangle:", area)
