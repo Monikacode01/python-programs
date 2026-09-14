d52b187031d8643c4c786b25418eab1cf600ec5f
Monika V <vmonika0120@gmail.com>
2026-09-14 22:13:01 +0530

    Create Convert_Fahrenheit_to_Celsius.py

---
+++ Convert_Fahrenheit_to_Celsius.py
@@ -0,0 +1,10 @@
+# Fahrenheit to Celsius Program
+
+# Get temperature in Fahrenheit
+fahrenheit = float(input("Enter temperature in Fahrenheit: "))
+
+# Convert Fahrenheit to Celsius
+celsius = (fahrenheit - 32) * 5/9
+
+# Display the result
+print("Temperature in Celsius:", celsius)
