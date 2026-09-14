05454624c73222a93d20e61fe99b793e21aeb61c
Monika V <vmonika0120@gmail.com>
2026-09-14 22:19:04 +0530

    Create Convert_miles_to_kilometers.py

---
+++ Convert_miles_to_kilometers.py
@@ -0,0 +1,10 @@
+# Miles to Kilometers Program
+
+# Get distance in miles
+miles = float(input("Enter distance in miles: "))
+
+# Convert miles to kilometers
+kilometers = miles * 1.60934
+
+# Display the result
+print("Distance in kilometers:", kilometers)
