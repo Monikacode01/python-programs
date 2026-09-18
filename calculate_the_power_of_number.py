198b4bde60ebb3891dfd78add14167336e3047f7
Monika V <vmonika0120@gmail.com>
2026-09-18 21:48:03 +0530

    Create calculate_the_power_of_number.py

---
+++ calculate_the_power_of_number.py
@@ -0,0 +1,11 @@
+# Power of a Number
+
+# Get the base and exponent from the user
+base = int(input("Enter the base: "))
+exponent = int(input("Enter the exponent: "))
+
+# Calculate the power
+result = base ** exponent
+
+# Display the result
+print("Result:", result)
