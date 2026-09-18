4aac896c11dbde016be826e695450d2fc5f17cc5
Monika V <vmonika0120@gmail.com>
2026-09-18 21:45:04 +0530

    Create print_cube_of_number.py

---
+++ print_cube_of_number.py
@@ -0,0 +1,10 @@
+# Cube of a Number
+
+# Get a number from the user
+num = int(input("Enter a number: "))
+
+# Calculate the cube
+cube = num ** 3
+
+# Display the result
+print("Cube of the number:", cube)
