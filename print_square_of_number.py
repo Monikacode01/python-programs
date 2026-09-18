0682e14a4ab52d20acb11eda3a897bd990385dd6
Monika V <vmonika0120@gmail.com>
2026-09-18 21:42:49 +0530

    Create print_square_of_number.py

---
+++ print_square_of_number.py
@@ -0,0 +1,10 @@
+# Square of a Number
+
+# Get a number from the user
+num = int(input("Enter a number: "))
+
+# Calculate the square
+square = num ** 2
+
+# Display the result
+print("Square of the number:", square)
